855d565821bf396206bb7dc59ed2ff0191b6bc71
VIPABH <a891983.2@gmail.com>
2025-03-21 15:15:24 +0300

    Update code.py

--- code.py
+++ code.py
@@ -56,7 +56,7 @@ async def handle_message(event):
         return
     state = user_states[user_id]
     step = state.get('step')
-    if step is None and iStart:
+    if step is None and not iStart:
         user_states[user_id]['step'] = 'get_subject'
         step = 'get_subject'
     if step == 'get_subject':
