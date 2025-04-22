8010e5ea41a9271035af72ca40fa7b7b018128fc
VIPABH <a891983.2@gmail.com>
2025-04-22 14:11:22 +0300

    Update code.py

--- code.py
+++ code.py
@@ -11,19 +11,20 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-CHANNEL_ID = -1002116581783
-الرابط = "https://t.me/x04ou"
 def is_user_subscribed(user_id):
     url = f"https://api.telegram.org/bot{bot_token}/getChatMember?chat_id={CHANNEL_ID}&user_id={user_id}"
     try:
         response = requests.get(url).json()
+        print("Response:", response)
         if response.get("ok"):
             status = response["result"]["status"]
+            print(f"User status: {status}")
             return status in ["member", "administrator", "creator"]
         else:
             print(f"Failed to get user status. Response: {response}")
             return False
     except requests.exceptions.RequestException as e:
+        print(f"Error making request: {e}")
         return False
 @ABH.on(events.NewMessage(incoming=True))
 async def handler(event):
@@ -31,9 +32,10 @@ async def handler(event):
         return
     user_id = event.sender_id
     if not is_user_subscribed(user_id):
+        channel_link = f"https://t.me/{CHANNEL_ID}"
         await event.respond(
-            f"👇لطفاً يجب ان تكون مشترك بالقناة",
-            buttons=[Button.url("القناة", الرابط)]
+            f"📌 للمتابعة، يرجى الاشتراك أولاً في القناة:\n{channel_link}",
+            buttons=[Button.url("اضغط هنا للاشتراك", channel_link)]
         )
         await event.delete()
         return
