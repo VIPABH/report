056f3d89d71563e99250e6cf84ecbc0909b80efc
VIPABH <a891983.2@gmail.com>
2025-04-22 14:02:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -10,6 +10,31 @@ default_smtp_port = 465
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
+CHANNEL_ID = -1002116581783
+الرابط = "https://t.me/x04ou"
+def is_user_subscribed(user_id):
+    url = f"https://api.telegram.org/bot{bot_token}/getChatMember?chat_id={CHANNEL_ID}&user_id={user_id}"
+    try:
+        response = requests.get(url).json()
+        if response.get("ok"):
+            status = response["result"]["status"]
+            return status in ["member", "administrator", "creator"]
+        else:
+            print(f"Failed to get user status. Response: {response}")
+            return False
+    except requests.exceptions.RequestException as e:
+        return False
+@ABH.on(events.NewMessage(incoming=True))
+async def handler(event):
+    if not event.is_private:
+        return
+    user_id = event.sender_id
+    if not is_user_subscribed(user_id):
+        await event.respond(
+            f"لطفاً يجب ان تكون مشترك بالقناة👇",
+            buttons=[Button.url("القناة", الرابط)]
+        )
+        return
 user_states = {}
 def create_email_message(subject, body, recipient):
     return f"Subject: {subject}\nTo: {recipient}\n\n{body}"
