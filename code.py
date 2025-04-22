5c908ae93535439bc0ac409daee1feb7dcdd3c24
VIPABH <a891983.2@gmail.com>
2025-04-22 14:03:46 +0300

    Update code.py

--- code.py
+++ code.py
@@ -10,6 +10,7 @@ default_smtp_port = 465
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
+ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
 CHANNEL_ID = -1002116581783
 الرابط = "https://t.me/x04ou"
 def is_user_subscribed(user_id):
